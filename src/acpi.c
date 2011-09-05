ac1a5ae16ab3e56f5a1611a6e0e586519161813c
Magnus Christensson <mch@virtutech.com>
2011-09-05 13:35:07 +0200

    Add CONFIG_VIRTUTECH_MODEL option for Virtutech Simics x86-440bx machine model.
    Signed-off-by: Magnus Christensson <mch@virtutech.com>

--- src/acpi.c
+++ src/acpi.c
@@ -226,12 +226,36 @@ static void piix4_fadt_init(struct pci_device *pci, void *arg)
     fadt->acpi_disable = PIIX4_ACPI_DISABLE;
     fadt->gpe0_blk = cpu_to_le32(PIIX4_GPE0_BLK);
     fadt->gpe0_blk_len = PIIX4_GPE0_BLK_LEN;
+    /* WBINVD + PROC_C1 + SLP_BUTTON + FIX_RTC + RTC_S4 */
+    fadt->flags = cpu_to_le32((1 << 0) | (1 << 2) | (1 << 5) | (1 << 6)
+                              | (1 << 7));
+
+}
+
+#define VIRTUTECH_ACPI_ENABLE   0xf0
+#define VIRTUTECH_ACPI_DISABLE  0xf1
+#define VIRTUTECH_GPE0_BLK      (PORT_ACPI_PM_BASE + 0x0c)
+#define VIRTUTECH_GPE0_BLK_LEN  4
+
+static void piix4_virtutech_fadt_init(struct pci_device *pci, void *arg)
+{
+    struct fadt_descriptor_rev1 *fadt = arg;
+    fadt->acpi_enable = VIRTUTECH_ACPI_ENABLE;
+    fadt->acpi_disable = VIRTUTECH_ACPI_DISABLE;
+    fadt->gpe0_blk = cpu_to_le32(VIRTUTECH_GPE0_BLK);
+    fadt->gpe0_blk_len = VIRTUTECH_GPE0_BLK_LEN;
+    /* WBINVD + PWR_BUTTON + SLP_BUTTON + FIX_RTC */
+    fadt->flags = cpu_to_le32((1 << 0) | (1 << 4) | (1 << 5) | (1 << 6));
+    /* Legacy devices and 8042 present */
+    fadt->ia32_boot_arch = 3;
 }
 
 static const struct pci_device_id fadt_init_tbl[] = {
     /* PIIX4 Power Management device (for ACPI) */
     PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_82371AB_3,
                piix4_fadt_init),
+    PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_82371AB_0,
+               piix4_virtutech_fadt_init),
 
     PCI_DEVICE_END
 };
@@ -274,8 +298,6 @@ build_fadt(struct pci_device *pci)
     fadt->plvl2_lat = cpu_to_le16(0xfff); // C2 state not supported
     fadt->plvl3_lat = cpu_to_le16(0xfff); // C3 state not supported
     pci_init_device(fadt_init_tbl, pci, fadt);
-    /* WBINVD + PROC_C1 + SLP_BUTTON + FIX_RTC + RTC_S4 */
-    fadt->flags = cpu_to_le32((1 << 0) | (1 << 2) | (1 << 5) | (1 << 6) | (1 << 7));
 
     build_header((void*)fadt, FACP_SIGNATURE, sizeof(*fadt), 1);
 
