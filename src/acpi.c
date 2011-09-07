52ecbb025efb71c737043448a5b63d984874aa61
Magnus Christensson <mch@virtutech.com>
2011-09-07 14:56:56 +0200

    Add CONFIG_ACPI_HPET option.
    The HPET table is only added to the ACPI tables if the
    CONFIG_ACPI_HPET option is set to 1 (which is the default).
    
    Signed-off-by: Magnus Christensson <mch@virtutech.com>

--- src/acpi.c
+++ src/acpi.c
@@ -721,7 +721,8 @@ acpi_bios_init(void)
     ACPI_INIT_TABLE(build_fadt(pci));
     ACPI_INIT_TABLE(build_ssdt());
     ACPI_INIT_TABLE(build_madt());
-    ACPI_INIT_TABLE(build_hpet());
+    if (CONFIG_ACPI_HPET)
+            ACPI_INIT_TABLE(build_hpet());
     ACPI_INIT_TABLE(build_srat());
     void *mcfg = build_mcfg();
     if (mcfg)
