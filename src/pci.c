b81a5079cc24f38545d6b9a54bbb4ff6b081d87a
Andreas Ragnerstam <aragners@woody.e.vtech>
2011-09-12 14:45:41 +0200

    Set ICH10 SATA-controller in AHCI-mode when AHCI-mode is choosen in bios.

--- src/pci.c
+++ src/pci.c
@@ -197,6 +197,13 @@ pci_probe_devices(void)
             dev->parent = parent;
             dev->rootbus = rootbus;
             u32 vendev = pci_config_readl(bdf, PCI_VENDOR_ID);
+            if (vendev == (PCI_VENDOR_ID_INTEL
+                            | (PCI_DEVICE_ID_INTEL_ICH10_SATA << 16))
+                    && CONFIG_AHCI) {
+                dprintf(1, "INTEL ICH10 SATA, change to AHCI-mode\n");
+                pci_config_writew(bdf, 0x90, 0x60);
+                vendev = pci_config_readl(bdf, PCI_VENDOR_ID);
+            }
             dev->vendor = vendev & 0xffff;
             dev->device = vendev >> 16;
             u32 classrev = pci_config_readl(bdf, PCI_CLASS_REVISION);
