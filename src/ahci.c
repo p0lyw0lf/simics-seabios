0acdc2d20e9602a8d0cdf01ff1db0597faac6d2d
Andreas Ragnerstam <aragners@woody.e.vtech>
2011-09-26 14:21:05 +0200

    Send one port command at a time to PxCMD and read register before writing so bios will not try to clear flags that have been set when issuing previous commands.

--- src/ahci.c
+++ src/ahci.c
@@ -337,7 +337,10 @@ ahci_port_reset(struct ahci_ctrl_s *ctrl, u32 pnr)
         if (!(val & (PORT_CMD_FIS_RX | PORT_CMD_START |
                      PORT_CMD_FIS_ON | PORT_CMD_LIST_ON)))
             break;
-        val &= ~(PORT_CMD_FIS_RX | PORT_CMD_START);
+        val &= ~PORT_CMD_START;
+        ahci_port_writel(ctrl, pnr, PORT_CMD, val);
+        val = ahci_port_readl(ctrl, pnr, PORT_CMD);
+        val &= ~PORT_CMD_FIS_RX;
         ahci_port_writel(ctrl, pnr, PORT_CMD, val);
         if (check_tsc(end)) {
             warn_timeout();
@@ -417,7 +420,10 @@ static struct ahci_port_s* ahci_port_realloc(struct ahci_port_s *port)
     ahci_port_writel(port->ctrl, port->pnr, PORT_FIS_ADDR, (u32)port->fis);
 
     cmd = ahci_port_readl(port->ctrl, port->pnr, PORT_CMD);
-    cmd |= (PORT_CMD_FIS_RX|PORT_CMD_START);
+    cmd |= PORT_CMD_FIS_RX;
+    ahci_port_writel(port->ctrl, port->pnr, PORT_CMD, cmd);
+    cmd = ahci_port_readl(port->ctrl, port->pnr, PORT_CMD);
+    cmd |= PORT_CMD_START;
     ahci_port_writel(port->ctrl, port->pnr, PORT_CMD, cmd);
 
     return port;
@@ -442,6 +448,7 @@ static int ahci_port_setup(struct ahci_port_s *port)
     ahci_port_writel(ctrl, pnr, PORT_CMD, cmd);
 
     /* spin up */
+    cmd = ahci_port_readl(ctrl, pnr, PORT_CMD);
     cmd |= PORT_CMD_SPIN_UP;
     ahci_port_writel(ctrl, pnr, PORT_CMD, cmd);
     end = calc_future_tsc(AHCI_LINK_TIMEOUT);
@@ -479,6 +486,7 @@ static int ahci_port_setup(struct ahci_port_s *port)
     }
 
     /* start device */
+    cmd = ahci_port_readl(ctrl, pnr, PORT_CMD);
     cmd |= PORT_CMD_START;
     ahci_port_writel(ctrl, pnr, PORT_CMD, cmd);
 
