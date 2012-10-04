31094ff6cf5343fb328320df9f9bc640b04c7b67
Magnus Christensson <magnus.christensson@intel.com>
2012-10-04 09:27:23 +0200

    Disable VT-d address translation during boot. This is needed for reboot to work properly if VT-d was enabled at the point of reboot. Discovered while rebooting Fedora 14.

--- src/pciinit.c
+++ src/pciinit.c
@@ -309,6 +309,19 @@ static void ich10_pm_init(struct pci_device *pci, void *arg)
     pci_config_writeb(bdf, 0x44, 0x80); // ACPI enabled, SCI IRQ 9
 }
 
+static void x58_vtd_init(struct pci_device *pci, void *arg)
+{
+    u16 bdf = pci->bdf;
+
+    /* Find VTBAR */
+    u32 vtbar = pci_config_readl(bdf, 0x180) & ~0xfff;
+
+    /* Clear the VT-d translation enable bit */
+    u32 gcmd_addr = vtbar + 0x18;
+    u32 gcmd = *(u32 *)gcmd_addr;
+    *(u32 *)gcmd_addr = gcmd & 0x7fffffff;
+}
+
 static const struct pci_device_id pci_device_tbl[] = {
     /* PIIX4 Power Management device (for ACPI) */
     PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_82371AB_3,
@@ -323,6 +336,8 @@ static const struct pci_device_id pci_device_tbl[] = {
                ich10_pm_init),
     PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_ICH10_3,
                ich10_pm_init),
+
+    PCI_DEVICE(PCI_VENDOR_ID_INTEL, 0x342e, x58_vtd_init),
 
     PCI_DEVICE_END,
 };
