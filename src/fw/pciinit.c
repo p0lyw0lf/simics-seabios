dc88189bdfb8b8afb046e24ebb8373916e79111d
Henrik Baarnhielm <henrik.baarnhielm@intel.com>
2015-10-28 12:19:02 +0100

    Add support for ICH10 Corporate which has different device-ids.

--- src/fw/pciinit.c
+++ src/fw/pciinit.c
@@ -295,6 +295,8 @@ static const struct pci_device_id pci_device_tbl[] = {
     /* STORAGE IDE */
     PCI_DEVICE_CLASS(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_ICH10_SATA,
                      PCI_CLASS_STORAGE_IDE, ich10_sata_ide_init),
+    PCI_DEVICE_CLASS(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_ICH10_SATAC,
+                     PCI_CLASS_STORAGE_IDE, ich10_sata_ide_init),
     PCI_DEVICE_CLASS(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_82371AB,
                      PCI_CLASS_STORAGE_IDE, piix_ide_setup),
     PCI_DEVICE_CLASS(PCI_VENDOR_ID_INTEL, PCI_ANY_ID,
