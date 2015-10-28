0a66050380d590b7be8e1cf3962bdf782d90635d
Henrik Baarnhielm <henrik.baarnhielm@intel.com>
2015-10-28 13:37:19 +0100

    Remove pci_slot_get_pirq IRQ rotation.
    Signed-off-by: Magnus Christensson <mch@virtutech.com>

--- src/pciinit.c
+++ src/pciinit.c
@@ -377,7 +377,7 @@ static void pci_bios_init_device(struct pci_device *pci)
     /* map the interrupt */
     int pin = pci_config_readb(bdf, PCI_INTERRUPT_PIN);
     if (pin != 0)
-        pci_config_writeb(bdf, PCI_INTERRUPT_LINE, pci_slot_get_irq(pci, pin));
+        pci_config_writeb(bdf, PCI_INTERRUPT_LINE, pic_irqs[pin - 1]);
 
     pci_init_device(pci_device_tbl, pci, NULL);
 
