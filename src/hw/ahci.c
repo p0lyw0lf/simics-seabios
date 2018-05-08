cdf721fb6d8ab409680aa621ba755b5d0da809fe
Henrik Bäärnhielm <henrik.baarnhielm@intel.com>
2018-05-08 16:02:10 +0200

    Clear upper 32-bits of pointers to memory.

--- src/hw/ahci.c
+++ src/hw/ahci.c
@@ -362,7 +362,7 @@ ahci_port_alloc(struct ahci_ctrl_s *ctrl, u32 pnr)
 
     ahci_port_writel(ctrl, pnr, PORT_LST_ADDR, (u32)port->list);
     ahci_port_writel(ctrl, pnr, PORT_FIS_ADDR, (u32)port->fis);
-    if (ctrl->caps & HOST_CAP_64) {
+    if ((ctrl->caps & HOST_CAP_64) || 1) {
         ahci_port_writel(ctrl, pnr, PORT_LST_ADDR_HI, 0);
         ahci_port_writel(ctrl, pnr, PORT_FIS_ADDR_HI, 0);
     }
